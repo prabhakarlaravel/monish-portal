213a5935b900c471953e49ea22753e0da64396b4
SaiMonish <tsaimonish@gmail.com>
2019-08-12 10:09:34 -0700

    removed all garbage data

--- index.php
+++ index.php
@@ -5,13 +5,12 @@
 </head>
 <body>
 <h1>This is a landing page</h1>
-<<<<<<< HEAD
+
 <a href="indexnew.php">Click here for new index page</a>
 <p>Hi how are you?</p>
 <p>I am doing good.</p>
-=======
 
 <h5>Hello jack how are doing?</h5>
->>>>>>> 62094e4b9512a22a8affba49c98029187b35f1e1
+
 </body>
 </html>
